3c1a79120cbdec426ecd01414254f27aabb0eac8
Laura <laurawatersalston@gmail.com>
2024-07-30 17:39:36 -0400

    client file setup

--- client/src-sw.js
+++ client/src-sw.js
@@ -19,6 +19,20 @@ const pageCache = new CacheFirst({
   ],
 });
 
+const imageCache = new CacheFirst({
+  cacheName: 'image-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxEntries: 50,
+      maxAgeSeconds: 30 * 24 * 60 * 60, 
+    }),
+  ],
+});
+
+
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
@@ -26,5 +40,7 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  ({ request }) => imageCache.handle({ request })
+); 
